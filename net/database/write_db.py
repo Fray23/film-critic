af09a7811df05196edc484ea947f034e1538f02e
Fray23 <mars6358@gmail.com>
2019-10-18 13:43:47 +0600

    Build a relationship for Moves and refactor write_db code

--- net/database/write_db.py
+++ net/database/write_db.py
@@ -1,53 +1,70 @@
 import csv
 from net.database.db import data_db as db
-from net.move.models import Move, Сompany, Genre, Country
+from net.move.models import Move, Company, Genre, Country
 from sqlalchemy import exists
 
 import ast
 
 
-def write_data_to_move_meta_table():
-    with open('datasets/movies_metadata.csv', 'rt') as f:
+def get_obj_from_json(json_list, model_name):
+    models = {
+        'production_companies': Company,
+        'genres': Genre,
+        'production_countries': Country
+    }
+    model = models[model_name]
+    name_list = [i['name'] for i in ast.literal_eval(json_list)]
+    model_objects = db.Session.query(model).filter(model.name.in_(name_list))
+    return model_objects
+
+
+def write_data_to_move_meta_table(path):
+    def add_new_obj_to_session(row, model_type):
+        models = {
+            'production_companies': Company,
+            'genres': Genre,
+            'production_countries': Country
+        }
+        if not model_type in ['production_companies', 'genres', 'production_countries']:
+            return
+
+        if row.get(model_type, None):
+            try:
+                model = models[model_type]
+                for i in ast.literal_eval(row[model_type]):
+                    if not db.Session.query(exists().where(model.name==i['name'])).scalar() and i['name'] != '':
+                        db.Session.add(model(
+                            name=i['name'],
+                            slug='-'.join(i['name'].split(' ')),
+                            ))
+            except Exception as e:
+                print('error ', e)
+                return
+
+
+    with open(path, 'rt') as f:
         spamreader = csv.DictReader(f, quotechar='\"')
         db.migrate()
 
         for row in spamreader:
-            if row.get('production_companies', None):
-                for i in ast.literal_eval(row['production_companies']):
-                    if not db.Session.query(exists().where(Сompany.name==i['name'])).scalar() and i['name'] != '':
-                        db.Session.add(Сompany(
-                            name=i['name'],
-                            slug='-'.join(i['name'].split(' ')),
-                        ))
-
-            if row.get('production_countries', None):
-                for i in ast.literal_eval(row['production_countries']):
-                    if not db.Session.query(exists().where(Country.name==i['name'])).scalar() and i['name'] != '':
-                        db.Session.add(Country(
-                            name=i['name'],
-                            slug='-'.join(i['name'].split(' ')),
-                        ))
-
-            if row.get('genres', None):
-                for i in ast.literal_eval(row['genres']):
-                    if not db.Session.query(exists().where(Genre.name==i['name'])).scalar() and i['name'] != '':
-                        db.Session.add(Genre(
-                            name=i['name'],
-                            slug='-'.join(i['name'].split(' '))
-                        ))
+            add_new_obj_to_session(row, 'production_companies')
+            add_new_obj_to_session(row, 'production_countries')
+            add_new_obj_to_session(row, 'genres')
             db.Session.commit()
 
 
-def write_data_to_move_table():
-    with open('datasets/movies_metadata.csv', 'rt') as f:
+def write_data_to_move_table(path):
+    with open(path, 'rt') as f:
         spamreader = csv.DictReader(f, quotechar='\"')
-        db.migrate()
+
         for row in spamreader:
-            db.Session.add(Move(
-                adult= row['adult'] == 'True',
+            if db.Session.query(exists().where(Move.title==row['original_title'])).scalar():
+                continue
+
+            move = Move(
+                adult=row['adult'] == 'True',
                 belongs_to_collection=row['belongs_to_collection'],
                 budget=int(row['budget']),
-                genres=row['genres'],
                 homepage=row['homepage'],
                 idd=row['id'],
                 imdb_id=row['imdb_id'],
@@ -56,17 +73,31 @@ def write_data_to_move_table():
                 overview=row['overview'],
                 popularity=row['popularity'],
                 poster_path=row['poster_path'],
-                production_companies=row['production_companies'],
-                production_countries=row['production_countries'],
                 release_date=row['release_date'],
                 revenue=row['revenue'],
                 runtime=row['runtime'],
                 spoken_languages=row['spoken_languages'],
                 status=row['status'],
                 tagline=row['tagline'],
-                title=row['title'],
+                title=row['original_title'],
                 video=row['video'],
                 vote_average=row['vote_average'],
                 vote_count=row['vote_count'],       
-            ))
+            )
+            try:
+                genres, companies, countries = (
+                    get_obj_from_json(row['genres'], model_name='genres'), 
+                    get_obj_from_json(row['production_companies'], model_name='production_companies'),
+                    get_obj_from_json(row['production_countries'], model_name='production_countries')
+                    )
+            except Exception as e:
+                print('error ', e)
+                continue
+
+            for genre, company, country in zip(genres, companies, countries):
+                move.genres.append(genre)
+                move.production_companies.append(company)
+                move.production_countries.append(country)
+
+            db.Session.add(move)
             db.Session.commit()
--- net/move/models.py
+++ net/move/models.py
@@ -1,5 +1,24 @@
 from net.database.db import data_db as db
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Table, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+move_country_table = Table('move_country', db.Base.metadata,
+    Column('move_pk', Integer, ForeignKey('moves.pk')),
+    Column('country_pk', Integer, ForeignKey('countries.pk'))
+)
+
+move_companies_table = Table('move_companies', db.Base.metadata,
+    Column('move_pk', Integer, ForeignKey('moves.pk')),
+    Column('companies_pk', Integer, ForeignKey('companies.pk'))
+)
+
+
+move_genres_table = Table('move_genres', db.Base.metadata,
+    Column('move_pk', Integer, ForeignKey('moves.pk')),
+    Column('genres_pk', Integer, ForeignKey('genres.pk'))
+)
+
 
 class Move(db.Base):
     __tablename__ = 'moves'
@@ -8,7 +27,7 @@ class Move(db.Base):
     adult                   = Column(Boolean)
     belongs_to_collection   = Column(String)
     budget                  = Column(Integer)
-    genres                  = Column(String)
+    genres                  = relationship('Genre', secondary=move_genres_table, backref='moves')
     homepage                = Column(String)
     idd                     = Column(String)
     imdb_id                 = Column(String)
@@ -17,8 +36,8 @@ class Move(db.Base):
     overview                = Column(String)
     popularity              = Column(String)
     poster_path             = Column(String)
-    production_companies    = Column(String)
-    production_countries    = Column(String)
+    production_companies    = relationship('Company', secondary=move_companies_table, backref='moves')
+    production_countries    = relationship('Country', secondary=move_country_table, backref='moves')
     release_date            = Column(String)
     revenue                 = Column(String)
     runtime                 = Column(String)
@@ -31,7 +50,7 @@ class Move(db.Base):
     vote_count              = Column(String)
 
     def __repr__(self):
-        return "<Move(pk='{}'>".format(self.pk)
+        return "<Move pk='{}'>".format(self.pk)
 
 
 class Country(db.Base):
@@ -43,7 +62,7 @@ class Country(db.Base):
     numerical_representation = Column(Integer)
 
     def __repr__(self):
-        return "<Country(pk='{}'>".format(self.pk)
+        return "<Country pk='{}', name='{}'>".format(self.pk, self.name)
 
 
 class Genre(db.Base):
@@ -55,13 +74,18 @@ class Genre(db.Base):
     numerical_representation = Column(Integer)
 
     def __repr__(self):
-        return "<Country(pk='{}'>".format(self.pk)
+        return "<Genre pk='{}', name='{}'>".format(self.pk, self.name)
 
 
-class Сompany(db.Base):
+class Company(db.Base):
     __tablename__ = 'companies'
 
     pk                       = Column(Integer, primary_key=True)
     name                     = Column(String)
     slug                     = Column(String)
     numerical_representation = Column(Integer)
+
+    def __repr__(self):
+        return "<Company pk='{}', name='{}'>".format(self.pk, self.name)
+
+db.migrate()
